8dd525d53afbf7f00aab2871d0bbf644f1644bee
Ramiro Bazan <ramirobazan35@gmail.com>
2025-10-21 01:16:27 -0300

    Base super basica(sin nada interesante)

---
+++ PaginaAgropecuaria/JavaScipt/main.js
@@ -0,0 +1,13 @@
+ocument.addEventListener("DOMContentLoaded",() => {
+    const window_name = document.;
+    if (window_name == "linux-info") {
+        const toggle = document.getElementById('menu-toggle');
+        const menu = document.getElementById('menu');
+        toggle.addEventListener('click', () => {
+            menu.classList.toggle('show');
+        });
+    } else {
+        alert
+        //alert("oh no");
+    }
+});
